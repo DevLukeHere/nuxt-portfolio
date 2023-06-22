ea0000ffa9c5ecd5424e2ecb970880f2235ed4e3
Luke Wong <lukewong93@gmail.com>
2023-06-22 23:19:36 +0800

    feat: display blog posts

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,10 @@
 <script setup>
+const { data: posts } = await useAsyncData('latest-posts', () =>
+  queryContent('/blog')
+    .sort({ data: 1 })
+    .limit(3)
+    .find()
+)
 </script>
 
 <template>
@@ -33,8 +39,8 @@
 
   <section>
     <h2 class="text-3xl font-bold mt-8">Latest Blog Posts</h2>
-    <!-- <div class="grid md:grid-cols-3 pt-8 gap-10">
+    <div class="grid md:grid-cols-3 pt-8 gap-10">
       <Post :posts="posts" />
-    </div> -->
+    </div>
   </section>
 </template>
