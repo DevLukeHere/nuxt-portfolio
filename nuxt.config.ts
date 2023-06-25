f90541c051c8d50f5abbf1935ace8a1e6fd1e133
Luke Wong <lukewong93@gmail.com>
2023-06-25 12:49:07 +0800

    build: configure github token

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxt/content"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "@nuxt/content",
+    "@nuxtjs/apollo",
+  ],
   content: {
     // https://content.nuxtjs.org/api/configuration
     highlight: {
       theme: "nord",
       preload: ["ts", "js", "css", "vue", "json"],
+    },
+  },
+  runtimeConfig: {
+    githubToken: process.env.GITHUB_TOKEN,
+  },
+  apollo: {
+    clients: {
+      default: {
+        tokenName: "github-token",
+        httpEndpoint: "https://api.github.com/graphql",
+      },
     },
   },
   devtools: { enabled: false },
