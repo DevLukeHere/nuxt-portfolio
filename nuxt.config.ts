cb1ebd04cdfb4ac012d27393e9df5508090112bd
Luke Wong <lukewong93@gmail.com>
2023-06-25 17:03:00 +0800

    build: configure nuxt apollo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxtjs/tailwindcss',
-    'nuxt-icon',
-    '@nuxt/content',
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "@nuxt/content",
+    "@nuxtjs/apollo",
   ],
   runtimeConfig: {
-    githubToken: process.env.GITHUB_TOKEN
+    githubToken: process.env.GITHUB_TOKEN,
   },
   content: {
     // https://content.nuxtjs.org/api/configuration
     highlight: {
-      theme: 'nord',
-      preload: ['ts','js','css','java','json','bash','vue']
-    }
+      theme: "nord",
+      preload: ["ts", "js", "css", "java", "json", "bash", "vue"],
+    },
   },
-})
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: 'https://api.github.com/graphql'
+      }
+    },
+  },
+});
