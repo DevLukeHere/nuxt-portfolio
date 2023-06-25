7638322b8e8fdde42a619f4e1be1d36148862857
Luke Wong <lukewong93@gmail.com>
2023-06-25 17:15:28 +0800

    refactor: remove double quote

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    "@nuxtjs/tailwindcss",
-    "nuxt-icon",
-    "@nuxt/content",
-    "@nuxtjs/apollo",
+    '@nuxtjs/tailwindcss',
+    'nuxt-icon',
+    '@nuxt/content',
+    '@nuxtjs/apollo'
   ],
   runtimeConfig: {
     githubToken: process.env.GITHUB_TOKEN,
